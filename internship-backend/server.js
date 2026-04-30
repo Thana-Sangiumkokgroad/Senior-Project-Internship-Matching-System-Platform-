573e67db6b816334b8e1869fd7cfd9056640e737
Thana-Sangiumkokgroad <thana@example.com>
2026-05-01 00:55:25 +0700

    db.js

--- internship-backend/server.js
+++ internship-backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const pool = require('./db');
 require('dotenv').config();
+const pool = require('./db');
 
 // Import routes
 const authRoutes = require('./routes/auth');
